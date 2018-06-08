a03338019607c2f1a8967077238d4e2454c84571
Mars <mars0925@24drs.com>
2018-06-08 14:09:49 +0800

    Log.d("AA","AA");

--- app/src/main/java/com/mars/android20171228/MainActivity.java
+++ app/src/main/java/com/mars/android20171228/MainActivity.java
@@ -29,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
         //引數放自己,目標類別
         it.putExtra( "data",ed.getText().toString());//得到目前頁面的資料
         startActivity(it);//開啟一個頁面(intent的一個方法)
-        Log.d("BB","BB");
+        Log.d("AA","AA");
+        Log.d("AA","AA");
     }
 
 
